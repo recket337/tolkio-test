21a6c53798a0c806122e2f9fd971ba4ef49c4d85
Ilia, Kuzin <gign.brest@gmail.com>
2023-03-20 14:35:35 +0100

    refactor: prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
-import React from 'react';
-import { FormMaker } from './components/FormMaker/FormMaker';
-import { AUTH_FORM_ID } from './constants';
-import { exampleConfig } from './mock';
+import React from "react";
+import { FormMaker } from "./components/FormMaker/FormMaker";
+import { AUTH_FORM_ID } from "./constants";
+import { exampleConfig } from "./mock";
 
 function App() {
   return (
     <div className="App">
-      <div className='fade-area'>
-        <FormMaker 
-          label='Auth' 
-          id={AUTH_FORM_ID} 
-          config={exampleConfig} 
-        />
-        <div className='button-container'>
-          <input type="submit" className='submit' form={AUTH_FORM_ID} value="Submit"/>
+      <div className="fade-area">
+        <FormMaker label="Auth" id={AUTH_FORM_ID} config={exampleConfig} />
+        <div className="button-container">
+          <input
+            type="submit"
+            className="submit"
+            form={AUTH_FORM_ID}
+            value="Submit"
+          />
         </div>
       </div>
     </div>
--- src/components/FormMaker/FormMaker.tsx
+++ src/components/FormMaker/FormMaker.tsx
@@ -1,59 +1,64 @@
-import React, { useEffect, useState } from 'react';
-import { useAppDispatch, useAppSelector } from '../../hooks';
-import { setFormData } from '../../slices/formStorageSlice';
-import { FormMakerPropsType, InputTypes } from './types';
-import { getInitialState } from './utils';
+import React, { useEffect, useState } from "react";
+import { useAppDispatch, useAppSelector } from "../../hooks";
+import { setFormData } from "../../slices/formStorageSlice";
+import { FormMakerPropsType, InputTypes } from "./types";
+import { getInitialState } from "./utils";
 
 export const FormMaker = ({ label, config, id }: FormMakerPropsType) => {
-    const [values, setValues] = useState<Record<string,string>>({});
+  const [values, setValues] = useState<Record<string, string>>({});
 
-    const authFormData = useAppSelector((state) => state.formStorage[id])
-    
-    const dispatch = useAppDispatch()
+  const authFormData = useAppSelector((state) => state.formStorage[id]);
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setValues(prev => ({
-            ...prev,
-            [e.target.id]: e.target.value
-        }))
-    }
+  const dispatch = useAppDispatch();
 
-    useEffect(() => {
-        dispatch(setFormData({
-            id,
-            data: values
-        }));
-    }, [dispatch, id, values])
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValues((prev) => ({
+      ...prev,
+      [e.target.id]: e.target.value,
+    }));
+  };
 
-    useEffect(() => {
-        const initialValue = getInitialState(config);
-        setValues(initialValue);
-        dispatch(setFormData({
-            id,
-            data: initialValue,
-        }));
-    }, [config, dispatch, id])
-
-    useEffect(() => {
-        console.log('redux', authFormData);
-    }, [authFormData])
-
-    return (
-      <form className="form" id={id}>
-        { label && <h3 className='form-label'>{label}</h3> }
-        {config.map(item => {
-            return <div className='inputField'>
-                <label htmlFor={item.id}>
-                    {item.label}
-                </label>
-                <input 
-                    type={InputTypes[item.type]} 
-                    id={item.id} onChange={handleChange} 
-                    value={values[item.id]}
-                    required={item.required}
-                />
-            </div>
-        })}
-      </form>
+  useEffect(() => {
+    dispatch(
+      setFormData({
+        id,
+        data: values,
+      })
     );
-  }
+  }, [dispatch, id, values]);
+
+  useEffect(() => {
+    const initialValue = getInitialState(config);
+    setValues(initialValue);
+    dispatch(
+      setFormData({
+        id,
+        data: initialValue,
+      })
+    );
+  }, [config, dispatch, id]);
+
+  useEffect(() => {
+    console.log("redux", authFormData);
+  }, [authFormData]);
+
+  return (
+    <form className="form" id={id}>
+      {label && <h3 className="form-label">{label}</h3>}
+      {config.map((item) => {
+        return (
+          <div className="inputField">
+            <label htmlFor={item.id}>{item.label}</label>
+            <input
+              type={InputTypes[item.type]}
+              id={item.id}
+              onChange={handleChange}
+              value={values[item.id]}
+              required={item.required}
+            />
+          </div>
+        );
+      })}
+    </form>
+  );
+};
